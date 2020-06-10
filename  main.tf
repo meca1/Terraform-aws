69ea3fe41b4c84077fdef320f30f51b980ebdf41
Daniel <drramosdev@gmail.com>
2020-06-09 23:14:28 -0500

    Terraform fmt: canonical format and style

---  main.tf
+++  main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "demo_akily_terraform" {
-	ami = var.ami_id 
-	instance_type = var.instance_type
-	tags = var.tags	
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags          = var.tags
 } 
--- env.tfvars
+++ env.tfvars
@@ -1,3 +1,3 @@
-ami_id = "ami-0e0054167ee726e37"
-instance_type =  "t2.micro"
-tags = { Name = "practica1", Enviroment="dev" }
+ami_id        = "ami-0e0054167ee726e37"
+instance_type = "t2.micro"
+tags          = { Name = "practica1", Enviroment = "dev" }
